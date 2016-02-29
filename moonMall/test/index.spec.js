a09f6119a901f97105a5e07a81e48ea4ea0bbfa8
zhengguorong <zhengguorong@bluemoon.com.cn>
2016-02-29 10:38:45 +0800

    添加获取预约信息接口测试用例

--- moonMall/test/index.spec.js
+++ moonMall/test/index.spec.js
@@ -20,5 +20,27 @@ describe('检票模块', function(res) {
 			}
 			done();
 		});
+	});
+	it('获取预约信息接口',function(done){
+		request("http://localhost:3000")
+			.get('/wechats/activity/venuePlan/findAllBooking')
+			.expect(200)
+			.end(function(err,res){
+				var data=res.body;
+				data.isSuccess.should.be.a.Boolean();
+				data.responseCode.should.be.a.Number();
+				data.responseMgs.should.be.a.String();
+				data.list.should.be.a.Array();
+				data.should.have.property('isSuccess',true);
+				data.should.have.property('responseCode',0);
+				if(data.list.length>0){
+					data.list[0].should.be.a.Object();
+					data.list[0].id.should.be.a.Number();
+					data.list[0].actDate.should.be.a.String();
+					//判断手机号长度
+					data.list[0].userId.should.have.length(11);
+				}
+				done();
+			})
 	})
 });
