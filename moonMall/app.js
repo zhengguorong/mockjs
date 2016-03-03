4c9a73fd1fdfc40d44a959f8e208bec472a64879
wq1031855643 <1031855643@qq.com>
2016-03-03 14:00:23 +0800

    mock数据

--- moonMall/app.js
+++ moonMall/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/ticket',ticket);
+app.use('/bluemoon-control/ticket',ticket);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
