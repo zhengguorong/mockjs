b5f1c58288604af41c5bed25964adf498943ec98
zhengguorong <zhengguorong@bluemoon.com.cn>
2016-05-06 13:05:40 +0800

    change

--- moonMall/app.js
+++ moonMall/app.js
@@ -14,14 +14,19 @@ var ticket = require('./routes/ticket');
 
 var app = express();
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
     next();
+  }
 });
+
 mongoose.connect('mongodb://localhost/mock');
 var db = mongoose.connection;
 db.on('error',function(){
