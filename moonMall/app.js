92169648a348eb6cc82298c2fa6eb2025de31a73
zhengguorong <zhengguorong@bluemoon.com.cn>
2016-05-06 13:01:42 +0800

    set allow origin

--- moonMall/app.js
+++ moonMall/app.js
@@ -14,6 +14,14 @@ var ticket = require('./routes/ticket');
 
 var app = express();
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 mongoose.connect('mongodb://localhost/mock');
 var db = mongoose.connection;
 db.on('error',function(){
